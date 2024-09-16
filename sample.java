32dc47c418ef94f1cdef9cdb2c67e5457be1058b
Larsters <diccidentss@gmail.com>
2024-09-16 12:34:59 +0200

    test java style issues

--- sample.java
+++ sample.java
@@ -2,7 +2,8 @@
 
 
 public class Sample {
-    public static void main(String[]args){
-    System.out.println("Hello, world!");
-    }
+public static void main(String[] args){
+System.out.println("Hello, world!");
 }
+}
+
