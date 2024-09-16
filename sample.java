2b7a35d4d74bcedc885a5f1b91d3e560fa15fb44
Larsters <diccidentss@gmail.com>
2024-09-16 13:30:59 +0200

    another sample

--- sample.java
+++ sample.java
@@ -1,9 +1,8 @@
 // bad code examples
 
-
-public class Sample {
+public class Sample{
 public static void main(String[] args){
-System.out.println("Hello, world!");
+int x=10;int y=20;int z=x+y;System.out.println("Sum is: "+z);if(x>y){System.out.println("X is greater than Y");}else{System.out.println("Y is greater than X");}
 }
 }
 
