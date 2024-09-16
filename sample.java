4c7bfdda95265890c6c6e702036462175469c5b5
Larsters <diccidentss@gmail.com>
2024-09-16 13:47:23 +0200

    testing

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@
 
 public class Sample{
 public static void main(String[] args){
-int x=10;int y=20;int z=x+y;System.out.println("Sum is: "+z);if(x>y){System.out.println("X is greater than Y");}else{System.out.println("Y is greater than X");}
+int x=5;int y=10;int z=x+y;System.out.println("Sum is: "+z);if(x>y){System.out.println("X is greater than Y");}else{System.out.println("Y is greater than X");}
 }
 }
 
