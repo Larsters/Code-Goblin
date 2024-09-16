d41746b132d8ea2e5945de948136e5e1396c01fe
Larsters <diccidentss@gmail.com>
2024-09-16 14:03:52 +0200

    testing languages

--- sample.java
+++ sample.java
@@ -2,7 +2,7 @@
 
 public class Sample{
 public static void main(String[] args){
-int x=10;int y=15;int z=x+y;System.out.println("Sum is: "+z);if(x>y){System.out.println("X is greater than Y");}else{System.out.println("Y is greater than X");}
+int x=10;int y=15;int z=x+y;System.out.println("Sum is: "+z);if(x>y){System.out.println("X is greater than Y");}else{System.out.println("Y больше X");}
 }
 }
 
